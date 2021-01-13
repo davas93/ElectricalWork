56bda886373b3d31428bb7afd4f7950399a77953
davas93 <vasin.da93@gmail.com>
2021-01-13 15:36:24 +0300

    modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,3 +1,32 @@
-const modal = () => {};
+const modal = () => {
+	const modalCallback = document.getElementById('callback'),
+		modalOverlay = document.querySelector('.modal-overlay');
+
+	const openModal = () => {
+		modalCallback.style.display = 'block';
+		modalCallback.classList.add('animate__animated', 'animate__fadeIn');
+		modalOverlay.style.display = 'block';
+	};
+
+	const closeModal = () => {
+		modalCallback.style.display = '';
+		modalOverlay.style.display = '';
+	};
+
+	document.addEventListener('click', (event) => {
+		let target = event.target;
+
+		if (target.matches('.fancyboxModal')) {
+			openModal();
+		}
+
+		if (
+			target.closest('.modal-close') ||
+			target.matches('.modal-overlay')
+		) {
+			closeModal();
+		}
+	});
+};
 
 export default modal;
