b17fc34a78e9252c271e7252e7965f7c7eaf3b48
davas93 <vasin.da93@gmail.com>
2021-01-14 20:09:59 +0300

    carousel

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -4,41 +4,68 @@ const carousel = () => {
 		slide = carousel.querySelectorAll('.carousel-item'),
 		arrows = document.querySelector('.services-arrow');
 
-	let slidesShow = 3,
-		slideWidth = 100 / slidesShow,
-		position = 0;
-
-	const prevSlider = () => {
-		if (position <= 0) {
-			position = slide.length - slidesShow;
+	const getCarousel = () => {
+		if (window.innerWidth >= 992) {
+			slidesShow = 3;
+			carousel.style.transform = ``;
 		}
-		if (position > 0) {
-			--position;
 
-			carousel.style.transform = `translateX(-${position * slideWidth}%)`;
+		if (window.innerWidth < 992) {
+			slidesShow = 2;
+			carousel.style.transform = ``;
 		}
-	};
 
-	const nextSlider = () => {
-		if (position < slide.length - slidesShow) {
-			++position;
-			if (position >= slide.length - slidesShow) {
-				position = 0;
+		if (window.innerWidth < 768) {
+			slidesShow = 1;
+			carousel.style.transform = ``;
+		}
+
+		let slidesShow;
+		let slideWidth = 100 / slidesShow,
+			position = 0;
+
+		const prevSlider = () => {
+			if (position <= 0) {
+				position = slide.length + 1 - slidesShow;
 			}
-			carousel.style.transform = `translateX(-${slideWidth * position}%)`;
-		}
+			if (position > 0) {
+				--position;
+
+				carousel.style.transform = `translateX(-${
+					position * slideWidth
+				}%)`;
+			}
+		};
+
+		const nextSlider = () => {
+			if (position < slide.length + 1 - slidesShow) {
+				++position;
+				if (position >= slide.length + 1 - slidesShow) {
+					position = 0;
+				}
+				carousel.style.transform = `translateX(-${
+					slideWidth * position
+				}%)`;
+			}
+		};
+
+		arrows.addEventListener('click', (event) => {
+			let target = event.target;
+
+			if (target.matches('.arrow-left')) {
+				prevSlider();
+			}
+
+			if (target.matches('.arrow-right')) {
+				nextSlider();
+			}
+		});
 	};
 
-	arrows.addEventListener('click', (event) => {
-		let target = event.target;
+	getCarousel();
 
-		if (target.matches('.arrow-left')) {
-			prevSlider();
-		}
-
-		if (target.matches('.arrow-right')) {
-			nextSlider();
-		}
+	window.addEventListener('resize', () => {
+		getCarousel();
 	});
 };
 
