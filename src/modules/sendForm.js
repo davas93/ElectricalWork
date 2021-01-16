4d28ef70b4280fad44b38ce8084ff71b11c09bdd
davas93 <vasin.da93@gmail.com>
2021-01-16 12:33:10 +0300

    form validation

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -18,43 +18,70 @@ const sendForm = () => {
 	};
 
 	const checkCorrectData = () => {
-		input.forEach((item) => {
-			if (!item.value) {
-				let error = generateError('Поле должно быть заполнено!');
-				item.parentNode.append(error);
-				item.style.borderColor = 'rgb(211, 27, 27)';
-			} else {
-				item.style.borderColor = '';
+		const validation = (value) => {
+			value = true;
+			input.forEach((item) => {
+				if (item.value === '') {
+					let error = generateError('Поле должно быть заполнено!');
+					item.parentNode.append(error);
+					item.style.borderColor = 'rgb(211, 27, 27)';
+					value = false;
+				} else {
+					item.style.borderColor = '';
+				}
+			});
+
+			if (
+				!/^(?!\s+$)[А-Яа-я\s]+$/g.test(input[0].value) &&
+				input[0].value !== ''
+			) {
+				let error = generateError(
+					'Введите корректные данные! (только русские буквы)'
+				);
+				input[0].parentNode.append(error);
+				input[0].style.borderColor = 'rgb(211, 27, 27)';
+				value = false;
 			}
+
+			if (input[0].value.length < 2 && input[0].value !== '') {
+				let error = generateError(
+					'Минимальное количество символов - 2'
+				);
+				input[0].parentNode.append(error);
+				input[0].style.borderColor = 'rgb(211, 27, 27)';
+				value = false;
+			}
+
+			if (input[0].value.length > 50 && input[0].value !== '') {
+				let error = generateError(
+					'Максимальное количество символов - 50'
+				);
+				input[0].parentNode.append(error);
+				input[0].style.borderColor = 'rgb(211, 27, 27)';
+				value = false;
+			}
+
+			if (input[1].value.length < 15 && input[1].value !== '') {
+				let error = generateError('Минимальное количество цифр - 9');
+				input[1].parentNode.append(error);
+				input[1].style.borderColor = 'rgb(211, 27, 27)';
+				value = false;
+			}
+
+			return value;
+		};
+
+		if (validation()) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	const clearInputs = () => {
+		input.forEach((item) => {
+			item.value = '';
 		});
-		if (
-			!/^(?!\s+$)[А-Яа-я\s]+$/g.test(input[0].value) &&
-			input[0].value !== ''
-		) {
-			let error = generateError(
-				'Введите корректные данные! (только русские буквы)'
-			);
-			input[0].parentNode.append(error);
-			input[0].style.borderColor = 'rgb(211, 27, 27)';
-		}
-
-		if (input[0].value.length < 2 && input[0].value !== '') {
-			let error = generateError('Минимальное количество символов - 2');
-			input[0].parentNode.append(error);
-			input[0].style.borderColor = 'rgb(211, 27, 27)';
-		}
-
-		if (input[0].value.length > 50 && input[0].value !== '') {
-			let error = generateError('Максимальное количество символов - 50');
-			input[0].parentNode.append(error);
-			input[0].style.borderColor = 'rgb(211, 27, 27)';
-		}
-
-		if (input[1].value.length < 15 && input[1].value !== '') {
-			let error = generateError('Минимальное количество цифр - 9');
-			input[1].parentNode.append(error);
-			input[1].style.borderColor = 'rgb(211, 27, 27)';
-		}
 	};
 
 	form.addEventListener('submit', (event) => {
@@ -62,12 +89,33 @@ const sendForm = () => {
 
 		clearErrors();
 
-		checkCorrectData();
+		if (checkCorrectData()) {
+			const formData = new FormData(form);
+			let body = {};
+
+			formData.forEach((val, key) => {
+				body[key] = val;
+			});
+			postData(body)
+				.then((response) => {
+					if (response.status !== 200) {
+						throw new Error('status network not 200');
+					}
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+		}
+
+		clearInputs();
 	});
+	const postData = (body) => {
+		return fetch('./server.php', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(body),
+		});
+	};
 };
 
 export default sendForm;
-
-//^[а-яА-Я\s]+
-
-//^(?!\s+$)[А-Яа-я\s]+$
